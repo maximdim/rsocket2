8c43f282d85c629099a174b1c36862ce2edb1a03
Dmitri Maximovich <maxim@maximdim.com>
2021-06-24 09:31:32 -0400

    File upload over rsocket

--- src/main/java/rsocket/backend/RSocketController.java
+++ src/main/java/rsocket/backend/RSocketController.java
@@ -1,7 +1,15 @@
 package rsocket.backend;
 
+import java.io.IOException;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.rsocket.RSocketRequester;
@@ -34,6 +42,16 @@ public class RSocketController {
     @MessageMapping("/backend/telemetry")
     public Mono<Void> telemetry(Flux<String> messages) {
         return messages.doOnNext(message -> logger.info("Telemetry received: " + message)).then();
+    }
+
+    @MessageMapping("/backend/fileUpload")
+    public Flux<String> fileUpload(Flux<DataBuffer> bufferFlux) throws IOException {
+        Path file = Files.createTempFile("", ".pdf");
+        logger.info("Writing to {}", file.toAbsolutePath());
+        AsynchronousFileChannel channel = AsynchronousFileChannel.open(file, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        AsynchronousFileChannel a = null;
+        return DataBufferUtils.write(bufferFlux, channel)
+                .map(b -> "CHUNK_COMPLETED");
     }
 
 }
--- src/main/java/rsocket/client/BackendService.java
+++ src/main/java/rsocket/client/BackendService.java
@@ -4,6 +4,11 @@ import java.time.Duration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
@@ -23,7 +28,7 @@ public class BackendService {
 
     private final RSocketRequester requester;
 
-    public BackendService(RSocketRequester.Builder builder, RSocketStrategies strategies) {
+    public BackendService(RSocketRequester.Builder builder, RSocketStrategies strategies) throws Exception {
         logger.info("id: {}, backend URL: {}", id, backendUrl);
 
         HttpClient httpClient = HttpClient.create()
@@ -36,12 +41,21 @@ public class BackendService {
                 .dataMimeType(MediaType.APPLICATION_JSON)
                 .transport(WebsocketClientTransport.create(httpClient, "/rsocket"));
 
-         Flux.interval(Duration.ofSeconds(10))
+        Flux.interval(Duration.ofSeconds(10))
                  //.onBackpressureLatest()
                  .map(tick -> "Telemetry from " + id + " tick " + tick)
                  .as(flux -> requester.route("/backend/telemetry").data(flux).retrieveFlux(Void.class))
                  .retry()
                  .subscribe();
+
+        Resource resource = new UrlResource("https://freeclassicebooks.com/Tolstoy/War%20and%20Peace.pdf");
+        Flux<DataBuffer> readFlux = DataBufferUtils.read(resource, new DefaultDataBufferFactory(), 4096)
+                .doOnNext(s -> logger.info("Sent..."));
+        requester.route("/backend/fileUpload")
+                .data(readFlux)
+                .retrieveFlux(String.class)
+                .doOnNext(s -> logger.info("Upload Status : {}", s))
+                .subscribe();
     }
 
 }
